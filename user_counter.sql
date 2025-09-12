d3779dd7102ac7b3c9cbd69b39ce001a17e94b9d
Hankin <jinchaoh@163.com>
2025-09-12 14:44:11 +0800

    Create user_counter.sql

---
+++ user_counter.sql
@@ -0,0 +1,26 @@
+WITH time_range AS (
+    SELECT 
+        TO_DATE('2025-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS') AS start_time,
+        TO_DATE('2025-09-12 23:59:59', 'YYYY-MM-DD HH24:MI:SS') AS end_time
+    FROM DUAL
+)
+SELECT 
+    u.ID AS 用户ID,
+    u.NAME AS 用户姓名,
+    u.TEL AS 用户电话,
+    d.STOCK_CODE AS 股票代码,
+    d.STOCK_NAME AS 股票名称,
+    CASE 
+        WHEN d.ENTRUST_DIRECTION = 1 THEN '买入'
+        WHEN d.ENTRUST_DIRECTION = 2 THEN '卖出'
+        ELSE '未知'
+    END AS 委托方向,
+    SUM(d.AMOUNT * d.PRICE) AS 成交总金额
+FROM T_DEAL d
+JOIN T_USER u ON u.ID = d.USERID
+CROSS JOIN time_range tr
+WHERE d.USERID IN (115483467, 1880560517) 
+  AND d.DEAL_TIME >= tr.start_time
+  AND d.DEAL_TIME <= tr.end_time
+GROUP BY u.ID, u.NAME, u.TEL, d.STOCK_CODE, d.STOCK_NAME, d.ENTRUST_DIRECTION
+ORDER BY u.ID, 成交总金额 DESC;
